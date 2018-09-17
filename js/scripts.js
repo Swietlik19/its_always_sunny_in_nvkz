cbc0f5e10dffb76fed2ce19797abca179d2c0f43
Lyubov Mukhacheva <citymnk@gmail.com>
2018-09-17 23:22:14 +0700

    few changes
    rewrite js code to more native, add some more a11y

--- js/scripts.js
+++ js/scripts.js
@@ -1,47 +1,59 @@
-$(function(){
-  var currentStoryName = "";
+let currentStoryName = "";
 
-  /* Смена оформления в зависимости от текущего времени суток */
-  var currentTime = new Date();
-  var currentHour = currentTime.getHours();
-  
+/* Смена цветовой схемы в зависимости от текущего времени суток */
+let changeColorScheme = function() {
+  let currentTime = new Date();
+  let currentHour = currentTime.getHours();
+
   if (currentHour >= 20 || currentHour < 6) {
-    $("body").addClass("bg--dark");
-    $(".main-content").addClass("bg--dark");
-    $(".main-header").addClass("main-header--dark");
-    $(".main-footer").addClass("main-footer--dark");
-    $(".main-header__navigator-item-ref").css("color","azure");
+    document.querySelector("body").classList.add("bg--dark");
+    document.querySelector(".main-content").classList.add("bg--dark");
+    document.querySelector(".main-header").classList.add("main-header--dark");
+    document.querySelector(".main-footer").classList.add("main-footer--dark");
+    let refObjs = document.querySelectorAll(".main-header__navigator-item-ref");
+    for (elem of refObjs) elem.style.color = "azure";
   }
+};
+changeColorScheme();
+
+/* Функция при клике по дням */
+let changeDay = function(elemDay) {
+  let viewer = document.getElementById("viewer_id");
+  let storyName = elemDay.id.replace("day-","");
+  let story = document.getElementById("story-" + storyName);
+  let whereToPlaceImg = document.querySelector(".main-content__viewer-img");
+  let idParent = elemDay.parentElement.id;
+  let whereStoryToPlaceIn = document.getElementById("between-weeks-" + idParent.substring(5,idParent.length));
   
-  $(".main-content__day").click(function(){
-    $(".main-content__viewer").removeClass(".main-content__viewer--visible");
-    $(".main-content__day").removeClass("main-content__day--pressed");
-    $(".main-content__viewer-story").empty();
-    var elem = this;
-    var viewer = document.getElementById("viewer_id");
-    var storyName = elem.id.replace("day-","");
-    var storyId ="story-" + storyName;
-    var whereToPlaceImg = document.getElementsByClassName("main-content__viewer-img");
-    var idParent = elem.parentElement.id;
-    var whereImgToPlaceIn = document.getElementById("between-weeks-" + idParent.substring(5,idParent.length));
-    if (currentStoryName == storyName) {
-      $(".main-content__viewer").addClass("main-content__viewer--hidden");
-      currentStoryName = "";
-    } else {
-      $("#" + storyId).clone().appendTo($(".main-content__viewer-story" ));
-      $(".main-content__viewer").appendTo(whereImgToPlaceIn);
-      $(".main-content__viewer").removeClass("main-content__viewer--hidden");
-      $(".main-content__viewer").addClass("main-content__viewer--visible");
-      $("#" + elem.id).addClass("main-content__day--pressed");
-      whereToPlaceImg[0].src = "img/story_images/gif_" + storyName + ".gif";
-      currentStoryName = storyName;
-    }
-  });
+  viewer.classList.remove("main-content__viewer--visible");
+  let dayObjs = document.querySelectorAll(".main-content__day");
+  for (elem of dayObjs) elem.classList.remove("main-content__day--pressed");
+  document.querySelector(".main-content__viewer-story").innerHTML = "";
   
-  $(".main-content__nav-item").click(function(){
-    $(".main-content__nav-item").removeClass("main-content__nav-item--active");
-    $(this).addClass("main-content__nav-item--active");
-    $(".main-content__month").removeClass("main-content__month--visible");
-    $("#" + this.id.replace("title-","month-")).addClass("main-content__month--visible");
-  });
-});
+  if (currentStoryName == storyName) {
+    viewer.classList.add("main-content__viewer--hidden");
+    currentStoryName = "";
+  } else {
+    /* Переносим историю дня в viewer если она есть, если нет - переносим пусто */
+    document.querySelector(".main-content__viewer-story" ).innerHTML = (story || "" != "") ? story.innerHTML : "";
+    whereStoryToPlaceIn.insertBefore(viewer, null);
+    viewer.classList.remove("main-content__viewer--hidden");
+    viewer.classList.add("main-content__viewer--visible");
+    elemDay.classList.add("main-content__day--pressed");
+    whereToPlaceImg.src = "img/story_images/gif_" + storyName + ".gif";
+    currentStoryName = storyName;
+  }
+};
+
+/* Функция при смене месяцев на навигаторе месяцев */
+let changeMonth = function(elemMonth) {
+  let navObjs = document.querySelectorAll(".main-content__nav-item");
+  for (elem of navObjs) elem.classList.remove("main-content__nav-item--active");
+  elemMonth.classList.add("main-content__nav-item--active");
+  
+  let monthObjs = document.querySelectorAll(".main-content__month");
+  for (elem of monthObjs) elem.classList.remove("main-content__month--visible");
+  
+  /* Расчитываем ID таблички с выбранным месяцем, делаем её видимой */
+  document.getElementById(elemMonth.id.replace("title-","month-")).classList.add("main-content__month--visible");
+};
